0975099728f3195265bf55f424bfdf872ce8d3ef
boodev03 <ngocthang.devweb@gmail.com>
2025-04-23 22:08:25 +0700

    update

--- modules/home/AchivementSection.tsx
+++ modules/home/AchivementSection.tsx
@@ -12,7 +12,7 @@ export default function AchivementSection() {
       whileInView={{ opacity: 1 }}
       viewport={{ once: true, margin: "-100px" }}
       transition={{ duration: 0.8 }}
-      className="mlg:h-[735px] pt-5 pb-[60px] mlg:py-20 mlg:pl-[60px] relative"
+      className="md:h-[735px] pt-5 pb-[60px] md:py-20 md:pl-[60px] relative"
     >
       <div className="h-full relative">
         <motion.div
@@ -21,9 +21,9 @@ export default function AchivementSection() {
           viewport={{ once: true, margin: "-100px" }}
           transition={{ duration: 0.7, delay: 0.2 }}
           className={cn(
-            "max-w-full w-[58%] min-w-[350px] z-10",
+            "max-w-full w-[80%] md:w-[58%] min-w-[350px] z-10",
             // Translate 30px = 1/2 padding of parent
-            "absolute mlg:relative top-full mlg:top-0 -right-6 sm:-right-10 mlg:right-0 -translate-y-[30px] sm:-translate-y-[30%] mlg:translate-y-0"
+            "absolute md:relative top-full md:top-0 -right-6 sm:-right-10 md:right-0 -translate-y-[30px] sm:-translate-y-[30%] md:translate-y-0"
           )}
           style={{
             clipPath: "polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)",
@@ -45,7 +45,7 @@ export default function AchivementSection() {
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true, margin: "-100px" }}
           transition={{ duration: 0.7, delay: 0.3 }}
-          className="hidden mlg:block absolute right-0 top-[100px] h-[475px] w-[80%] bg-web-vivid z-[1]"
+          className="hidden md:block absolute right-0 top-[100px] h-[475px] w-[80%] bg-web-vivid z-[1]"
           style={{
             clipPath: "polygon(25% 0%, 100% 0%, 100% 100%, 0% 100%)",
           }}
@@ -55,7 +55,7 @@ export default function AchivementSection() {
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.6, delay: 0.5 }}
-            className="absolute top-[120px] right-[150px] w-[358px] space-y-6"
+            className="absolute top-[120px] right-[2%] mlg:right-[15%] w-[358px] space-y-6"
           >
             <motion.h3
               initial={{ opacity: 0 }}
@@ -109,7 +109,7 @@ export default function AchivementSection() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, margin: "-100px" }}
           transition={{ duration: 0.7, delay: 0.3 }}
-          className="bg-web-vivid block sm:hidden py-[60px] px-0 sm:px-[15%]"
+          className="bg-web-vivid md:hidden aspect-[375/358] flex justify-center items-center px-0 sm:px-[15%]"
           style={{
             clipPath: "polygon(0% 25%, 20% 0%, 100% 0%, 100% 100%, 0% 100%)",
           }}
@@ -119,14 +119,14 @@ export default function AchivementSection() {
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.6, delay: 0.5 }}
-            className="space-y-6 max-w-[75%] mx-auto"
+            className="max-w-[75%] space-y-2 mx-auto"
           >
             <motion.h3
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               viewport={{ once: true }}
               transition={{ duration: 0.5, delay: 0.6 }}
-              className="relative text-white font-bold tracking-[0.02em] text-base leading-[1.6] uppercase"
+              className="relative text-white font-bold tracking-[0.02em] text-base leading-[1.625] uppercase"
             >
               ACHIVEMENT
               <div
@@ -134,96 +134,35 @@ export default function AchivementSection() {
                 style={{ clipPath: "polygon(50% 0%, 100% 82%, 0% 82%)" }}
               />
             </motion.h3>
-            <motion.p
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-              className="text-white font-bold tracking-[0.02em] text-[32px] leading-[1.6]"
-            >
-              開発事例
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.8 }}
-              className="text-white tracking-[0.02em] text-base leading-[1.6]"
-            >
-              設計から溶接、隔離技術とワンストップで高品質な製品開発をオーダーメイドで行っています。
-            </motion.p>
+            <div className="space-y-3">
+              <motion.p
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.7 }}
+                className="text-white font-bold tracking-[0.02em] text-[20px] leading-[1.625]"
+              >
+                開発事例
+              </motion.p>
+              <motion.p
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.8 }}
+                className="text-white tracking-[0.02em] text-[15px] leading-[1.625]"
+              >
+                設計から溶接、隔離技術とワンストップで高品質な製品開発をオーダーメイドで行っています。
+              </motion.p>
+            </div>
 
             <motion.div
               initial={{ opacity: 0, y: 10 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
               transition={{ duration: 0.5, delay: 0.9 }}
+              className="mt-6"
             >
               <Button className="bg-white text-web-main hover:bg-web-main hover:text-white">
-                開発事例を見る
-                <ArrowRight />
-              </Button>
-            </motion.div>
-          </motion.div>
-        </motion.div>
-
-        {/* Tablet */}
-        <motion.div
-          initial={{ opacity: 0, y: 30 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true, margin: "-100px" }}
-          transition={{ duration: 0.7, delay: 0.3 }}
-          className="bg-web-vivid hidden sm:block mlg:hidden py-[60px] px-0 sm:px-[15%]"
-          style={{
-            clipPath: "polygon(0% 25%, 10% 0%, 100% 0%, 100% 100%, 0% 100%)",
-          }}
-        >
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay: 0.5 }}
-            className="space-y-6 max-w-[75%] mx-auto"
-          >
-            <motion.h3
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.6 }}
-              className="relative text-white font-bold tracking-[0.02em] text-base leading-[1.6] uppercase"
-            >
-              ACHIVEMENT
-              <div
-                className="absolute top-1 left-2 -translate-x-full size-[30px] bg-web-light -z-[1] -rotate-[135deg]"
-                style={{ clipPath: "polygon(50% 0%, 100% 82%, 0% 82%)" }}
-              />
-            </motion.h3>
-            <motion.p
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-              className="text-white font-bold tracking-[0.02em] text-[32px] leading-[1.6]"
-            >
-              開発事例
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.8 }}
-              className="text-white tracking-[0.02em] text-base leading-[1.6]"
-            >
-              設計から溶接、隔離技術とワンストップで高品質な製品開発をオーダーメイドで行っています。
-            </motion.p>
-
-            <motion.div
-              initial={{ opacity: 0, y: 10 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.9 }}
-            >
-              <Button className="bg-white text-web-main">
                 開発事例を見る
                 <ArrowRight />
               </Button>
